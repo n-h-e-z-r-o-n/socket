f43b44141db58f77344e95a510803b939f6b4f3e
Hezron Na <hezron.w12@gmail.com>
2023-06-08 11:32:15 +0300

    Committed 2023 0Th.113215

--- version 2/server_client/Concurrent_Connection_Oriented/server.c
+++ version 2/server_client/Concurrent_Connection_Oriented/server.c
@@ -64,6 +64,7 @@ int main() {
     char buffer[1024] = {0};
     char* substrings[3];
 
+    printf("Creating server socket");
     // Create a socket for the server and Bind the socket to a specific port.
     server_fd = socket(AF_INET, SOCK_STREAM, 0) ;
     address.sin_family = AF_INET;
@@ -78,7 +79,7 @@ int main() {
     while (1) {
         new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen);
 
-        int pid = fork(); //	Fork a child process 
+        int pid = fork(); //	Fork a child process
 
         if (pid == 0) { // Child process
             close(server_fd);
